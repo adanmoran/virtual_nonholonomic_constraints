59488a22c15a106a8052c83d544cd236028fb4c7
Adan Moran-MacDonald <adanmoranm@gmail.com>
2020-02-06 14:30:22 -0500

    Added a header file for including in the library for the Arduino to use this.

---
+++ sugar_bot/arduino/libraries/SUGAR_VNHC_Library/SUGAR_VNHC.h
@@ -0,0 +1,26 @@
+/*******************************************************************************
+* File:	        SUGAR_VNHC.h
+*		 
+* Author:       Adan Moran-Macdonald
+* Created:      06/Feb/20
+*
+* Systems Control Group
+* Department of Electrical and Computer Engineering
+* University of Toronto
+*
+* Last Modified: 06/Feb/20
+* Last Editor:   Adan Moran-Macdonald
+* Description:   Header file to include to use this library in the arduino. It
+* simply includes all the relevant headers here.
+*******************************************************************************/
+
+#ifndef __SUGAR_VNHC_H__
+#define __SUGAR_VNHC_H__
+
+#include "include/StateSpace.h"
+#include "include/AcrobotDynamics.h"
+#include "include/Phase.h"
+#include "include/VNHC.h"
+
+#endif
+/* vim: set tw=80 ts=4 sw=4 sts=0 et ffs=unix : */
